62cbd33d7e6840466dbe795950cd464afd9b34d7
Souichi Takamura <takamura_s@worksap.co.jp>
2017-02-21 01:09:41 +0000

    add elixir_v8 to deps

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule PhoenixReact.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:elixir_v8, github: "le0pard/elixir_v8"},
      {:cowboy, "~> 1.0"}]
   end
 
